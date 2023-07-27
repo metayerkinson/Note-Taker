3a51ca61daae3bbd4186cb697a5529d9aae3566c
Your Name <metayerkinson@gmail.com>
2023-07-27 13:34:25 -0400

    first commit

---
+++ Develop/server.js
@@ -0,0 +1,104 @@
+//require all dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+// const cors = require("cors");
+var PORT = process.env.PORT || 8000;
+app.use(express.static(__dirname + "/public"));
+// app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//direct user to correct page depending on url
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+//send json of all notes if user accesses /api/notes
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
+    if (error) {
+      return console.log(error);
+    }
+    res.json(JSON.parse(notes));
+  });
+});
+
+//use POST method to bring user input to backend
+app.post("/api/notes", (req, res) => {
+  //declare const for the note currently being saved by user
+  const currentNote = req.body;
+  //retrieve notes from db.json, get id of last note, add 1 to it to create
+  //new id, save current note with new id
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
+    if (error) {
+      return console.log(error);
+    }
+    notes = JSON.parse(notes);
+    //assign unique id to each new note depending on last id.
+    //if no items in notes array, assign id as 10
+    if (notes.length > 0) {
+      let lastId = notes[notes.length - 1].id;
+      var id = parseInt(lastId) + 1;
+    } else {
+      var id = 10;
+    }
+    //create new note object
+    let newNote = {
+      title: currentNote.title,
+      text: currentNote.text,
+      id: id,
+    };
+    //merge new note with existing notes array
+    var newNotesArr = notes.concat(newNote);
+    //write new array to db.json file and return it to user
+    fs.writeFile(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify(newNotesArr),
+      (error, data) => {
+        if (error) {
+          return error;
+        }
+        console.log(newNotesArr);
+        res.json(newNotesArr);
+      }
+    );
+  });
+});
+
+//delete chosen note using delete http method
+app.delete("/api/notes/:id", (req, res) => {
+  let deleteId = JSON.parse(req.params.id);
+  console.log("ID to be deleted: ", deleteId);
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
+    if (error) {
+      return console.log(error);
+    }
+    let notesArray = JSON.parse(notes);
+    //loop through notes array and remove note with id matching deleteId
+    for (var i = 0; i < notesArray.length; i++) {
+      if (deleteId == notesArray[i].id) {
+        notesArray.splice(i, 1);
+
+        fs.writeFile(
+          path.join(__dirname, "./db/db.json"),
+          JSON.stringify(notesArray),
+          (error, data) => {
+            if (error) {
+              return error;
+            }
+            console.log(notesArray);
+            res.json(notesArray);
+          }
+        );
+      }
+    }
+  });
+});
+
+//initialize port to start listening
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
---
+++ README.md
@@ -0,0 +1,46 @@
+# Note Taker Starter Code
+
+# Note Taker App
+
+A Note taking app build using Node.js and express.js for the purposes of writing down and saving notes.
+
+## User Story
+
+AS A small business owner
+I WANT to be able to write and save notes
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+
+## Description
+
+This application allows the user to input notes as well as a title for each note and save them. Saved notes appear in the left hand sidebar and if clicked on will be displayed in the main note section. Unwanted notes can also be deleted with the provided delete button.
+This app made use of Node.js, Express.js and javascript to create the front and backend of the application. It has been deployed using [Heroku]
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing Guidelines](#contributing)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+To run the app from your local machine, you must download all files and initialize the PORT from within the app folder using 'npm start' in the command line. Once PORT has been initialized, app can be opened in browser by navigating to 'localhost:3001'.
+For an easier method of accessing the app, use the heroku link.
+
+## Usage
+
+On opening the app, the user will be presented with a landing page with a button to 'get started'. On clicking the button, the user will be taken to the notes app where user can enter a note with a title and save it. Saved notes will be displayed in the left-hand sidebar and on clicking a note in the sidebar, it will be displayed in the main notes section. Clicking the pencil icon in the tp right hand corner will clear all input fields so new notes can be entered. If a note in no longer needed, it can be deleted using the delete icon next to each note in the sidebar.
+![landing page demo](images/demo1.png)
+
+![note taking demo](images/demo2.png)
+
+![note taking demo](images/demo3.png)
+
+## Questions
+
+The repo for this project can be found here:
+
+Heroku link :
+
+For any questions or to report issues, email me at:
